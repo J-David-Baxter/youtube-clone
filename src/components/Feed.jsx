262505eb0e0bd11f62f9456f46bfc245faf6be44
J-David-Baxter <jdavidbx@gmail.com>
2023-01-31 00:05:10 -0500

    Add content to feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Sidebar from './Sidebar';
+import Videos from './Videos';
 
 const Feed = () => {
   return (
@@ -12,6 +13,12 @@ const Feed = () => {
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 Video Share
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: "white" }}>
+          New <span style={{ color: "#fc1503"}}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
